9dfd3170f516d75d299cc5ac5ce3c714664208c6
Dominika Matyja <domka_24@interia.pl>
2020-01-14 14:10:58 +0100

    part 2

---
+++ SplitJoin/SplitJoin.py
@@ -0,0 +1,14 @@
+#Lista rozdzielona przecinkami
+cechy = "niski, zabawny, usmiechniety, inteligentny"
+print(cechy)
+print("\n******************\n")
+
+#Ustawianie znacznika podziału na ", "
+splited = cechy.split(", ")
+print(splited)
+print("\n******************\n")
+
+#Łączenie listy przy pomocy wybranego znacznika
+joined = " i ".join(splited)
+print(joined)
+print("\n******************\n")
